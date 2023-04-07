cf350ca3d224529f4e85283aad72fc82e401695f
saeedshpc <saeedsh.dev@gmail.com>
2023-04-08 01:54:25 +0430

    making svg into blade component

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -167,20 +167,7 @@
                         placeholder="جستجو..."
                         class="w-full focus:outline-none text-xs dark:bg-slate-700 dark:text-white"
                     />
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="w-6 h-6 dark:text-white"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-                        />
-                    </svg>
+                    <x-icon name="magnifier" />
                 </div>
                 <a
                     href="./add-tankhah.html"
@@ -190,20 +177,8 @@
                 <a
                     href="#"
                     class="p-2 px-2 rounded-lg bg-white dark:bg-slate-700 dark:text-white border dark:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-600 duration-200 text-sm"
-                ><svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="w-6 h-6"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
-                        />
-                    </svg>
+                >
+                   <x-icon name="pencil" />
                 </a>
             </header>
             {{$slot}}
