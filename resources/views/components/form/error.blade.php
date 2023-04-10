1fcb716d32bf495ad9177ae77dc51e4d12184c3f
saeedshpc <saeed.shpc@gmail.com>
2023-04-10 15:54:46 +0430

    adding error text and input style for form input fields

---
+++ resources/views/components/form/error.blade.php
@@ -0,0 +1,7 @@
+@props(['name'])
+
+@error($name)
+<p class="text-xs text-red-500 mt-2 pr-2">
+    {{ $message }}
+</p>
+@enderror
--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,5 +1,17 @@
 @props(['name', 'label' => 'برچسب', 'description' => 'توضیحات', 'seperator' => true ])
 
+@php
+    $classes = "w-full lg:w-96 dark:text-white
+    focus:outline-none text-sm p-4 px-4 rounded-lg border
+     focus:outline dark:bg-slate-700 ";
+
+    $withErrorStyles = "border-red-200 bg-red-200 bg-opacity-20 focus:border-red-400 focus:outline-red-100";
+    $withoutErrorStyle = "bg-white border-gray-200 focus:border-[#0b9b82] focus:outline-green-100";
+
+
+@endphp
+
+
 <div
     class="flex flex-col gap-4 items-start justify-between lg:flex-row lg:items-center lg:gap-0"
 >
@@ -15,9 +27,10 @@
         <input
             id="{{$name}}"
             name="{{$name}}"
-            class="w-full lg:w-96 dark:text-white bg-white focus:outline-none text-sm p-4 px-4 rounded-lg border border-gray-200 focus:border-[#0b9b82] focus:outline focus:outline-green-100 dark:bg-slate-700"
+            class="{{$classes}} @error($name) {{$withErrorStyles}} @else {{$withoutErrorStyle}} @enderror"
             {{$attributes(['value' => old($name)])}}
         />
+        <x-form.error name="{{$name}}" />
     </div>
 </div>
 @if($seperator)
