abe5a471ec258ad12d443f3769827f647f6c745e
saeedshpc <saeedsh.dev@gmail.com>
2023-04-16 13:47:48 +0430

    bug fix for latest commit expense index page

--- resources/views/expenses/index.blade.php
+++ resources/views/expenses/index.blade.php
@@ -3,7 +3,7 @@
     <div
         class="flex flex-col md:flex-row md:items-center justify-between gap-4 py-6 px-2 md:px-4"
     >
-        <x-reports-title title="مدیریت تنخواه" :records="$freelancerExpense->count()"/>
+        <x-reports-title title="مدیریت تنخواه" :records="$expenses->count()"/>
         <x-reports-filter/>
     </div>
     <!-- boxes -->
@@ -27,7 +27,7 @@
                 <x-table-th>تاریخ ثبت</x-table-th>
                 <x-table-th></x-table-th>
             </x-slot>
-            @foreach($freelancerExpense as $expense)
+            @foreach($expenses  as $expense)
                 @php
                     $statusColor = '';
                      switch($expense->payment_status) {
