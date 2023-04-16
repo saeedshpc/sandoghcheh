18457b4897e2ae028697ff9b83533d508d8fde9f
saeedshpc <saeedsh.dev@gmail.com>
2023-04-16 19:32:58 +0430

    adding create blade for advertising expenses

--- app/Http/Controllers/AdvertisingExpenseController.php
+++ app/Http/Controllers/AdvertisingExpenseController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Advertiser;
 use App\Models\AdvertisingExpense;
+use App\Models\BankAccount;
+use App\Models\Company;
 use Illuminate\Http\Request;
 
 class AdvertisingExpenseController extends Controller
@@ -16,12 +19,16 @@ class AdvertisingExpenseController extends Controller
 
     public function create()
     {
-
+        return view('advertisingExpenses.create', [
+            'companies' => Company::all(),
+            'bankAccounts' => BankAccount::all(),
+            'advertisers' => Advertiser::all(),
+        ]);
     }
 
     public function store()
     {
-
+        dd(request()->all());
     }
 
     public function edit(AdvertisingExpense $advertisingExpense)
