c2aa37549bb9f13f1fb439c9d1fe8d7843504d39
saeedshpc <saeedsh.dev@gmail.com>
2023-04-17 19:12:16 +0430

    store method for revenue

--- app/Http/Controllers/RevenueController.php
+++ app/Http/Controllers/RevenueController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Company;
 use App\Models\Revenue;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class RevenueController extends Controller
 {
@@ -24,7 +25,20 @@ class RevenueController extends Controller
 
     public function store()
     {
-        dd(request()->all());
+        $attributes = request()->validate([
+            'company_id' => ['required', Rule::exists('companies', 'id')],
+            'month_of_year' => ['required'],
+            'total_sale' => ['required', 'integer'],
+            'online_sale_share' => ['required', 'integer'],
+            'offline_sale_share' => ['required', 'integer'],
+            'description' => [],
+        ]);
+
+        Revenue::create($attributes);
+
+        return redirect('/revenues')->with([
+            'message' => 'درآمد جدید برای یک کسب و کار ثبت شد'
+        ]);
     }
 
 }
