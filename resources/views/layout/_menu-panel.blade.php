27ff9b097ef76258a4aaa35423bb20fa161c25dc
saeedshpc <saeedsh.dev@gmail.com>
2023-04-22 20:25:18 +0430

    seperating menu panel and mobile panel and making svg's into component

---
+++ resources/views/layout/_menu-panel.blade.php
@@ -0,0 +1,155 @@
+<aside
+    dir="rtl"
+    class="hidden md:block bg-white col-span-3 xl:col-span-2 py-6 px-2 lg:px-6 dark:bg-slate-800 dark:text-white"
+>
+    <img src="/images/logo.png" class="scale-50 -mt-5" alt=""/>
+    <h2 class="text-center -mt-4 font-bold text-xl mb-1">صندوقچه</h2>
+    <p class="text-xs tb:text-sm text-center">
+        سامانه مالی توکان تجارت آرسس
+    </p>
+
+    <!-- menu 1 -->
+    <div class="mt-10 mr-2 xl:10 text-xs tb:text-sm">
+        <ul
+            class="flex flex-col gap-6 font-bold text-gray-400 dark:text-gray-300"
+        >
+            <li class="hover:text-gray-800 dark:hover:text-gray-400">
+                <a href="/expenses" class="flex items-center gap-2">
+                   <x-icon name="home" />
+                    <span>داشبورد</span></a
+                >
+            </li>
+            <li
+                class="text-gray-800 dark:text-white font-bold dark:hover:text-gray-400"
+            >
+                <a
+                    href="./tankhah-dashboard.html"
+                    class="flex items-center gap-2"
+                >
+                    <x-icon name="expenses" />
+                    <span>مدیریت تنخواه</span></a
+                >
+            </li>
+            <li class="hover:text-gray-800 dark:hover:text-gray-400">
+                <a
+                    href="/freelancerExpenses"
+                    class="flex items-center gap-2"
+                >
+                   <x-icon name="freelancerExpense" />
+                    <span>مدیریت فریلسنرها</span></a
+                >
+            </li>
+            <li class="hover:text-gray-800 dark:hover:text-gray-400">
+                <a href="./ads-dashboard.html" class="flex items-center gap-2">
+                    <x-icon name="advertismentExpense" />
+                    <span>مدیریت تبلیغات</span></a
+                >
+            </li>
+        </ul>
+    </div>
+    <div class="border-b-2 border-b-gray-100 my-10 mx-3"></div>
+    <!-- menu 2 -->
+    <div class="mt-10 mr-2 xl:10 text-xs tb:text-sm">
+        <ul
+            class="flex flex-col gap-6 font-bold text-gray-400 dark:text-gray-300"
+        >
+            <li class="hover:text-gray-800 dark:hover:text-gray-400">
+                <a href="./settings.html" class="flex items-center gap-2">
+                    <x-icon name="setting" />
+                    <span>تنظیمات</span></a
+                >
+            </li>
+            <li class="hover:text-gray-800 dark:hover:text-gray-400">
+                <a href="./companies" class="flex items-center gap-2">
+                    <x-icon name="companies" />
+                    <span>مدیریت شرکت‌ها</span></a
+                >
+            </li>
+
+            <li class="hover:text-gray-800 dark:hover:text-gray-400">
+                <a href="./cards" class="flex items-center gap-2">
+                    <x-icon name="bankAccounts"/>
+                    <span>مدیریت حساب‌های بانکی</span></a
+                >
+            </li>
+            <li class="hover:text-gray-800 dark:hover:text-gray-400">
+                <a href="./freelancers.html" class="flex items-center gap-2">
+                   <x-icon name="freelancers" />
+                    <span>همکاران فریلنسر</span></a
+                >
+            </li>
+        </ul>
+    </div>
+    <div class="border-b-2 border-b-gray-100 my-10 mx-3"></div>
+    <!-- menu 3 -->
+    <div class="mt-10 mr-2 xl:10 text-xs tb:text-sm">
+        <ul
+            class="flex flex-col gap-6 font-bold text-gray-400 dark:text-gray-300"
+        >
+            <li class="hover:text-gray-800 dark:hover:text-gray-400">
+                <a href="./incomes.html" class="flex items-center gap-2">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z"
+                        />
+                    </svg>
+                    <span>درآمد و نقدینگی</span></a
+                >
+            </li>
+            <li class="hover:text-gray-800 dark:hover:text-gray-400">
+                <a href="./advertisers.html" class="flex items-center gap-2">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5m.75-9l3-3 2.148 2.148A12.061 12.061 0 0116.5 7.605"
+                        />
+                    </svg>
+
+                    <span>مجریان تبلیغات</span></a
+                >
+            </li>
+            <li class="hover:text-gray-800 dark:hover:text-gray-400">
+                <a href="./activities.html" class="flex items-center gap-2">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                        />
+                    </svg>
+                    <span>فعالیت‌ها</span></a
+                >
+            </li>
+            <li class="hover:text-gray-800 dark:hover:text-gray-400">
+                <a href="./admins.html" class="flex items-center gap-2">
+                    <x-icon name="users" />
+                    <span>مدیریت کاربران سامانه</span></a
+                >
+            </li>
+        </ul>
+    </div>
+</aside>
