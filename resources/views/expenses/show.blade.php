fd63d3c3a621662871a43feec832c1e3c3608420
saeedshpc <saeedsh.dev@gmail.com>
2023-04-28 19:51:17 +0430

    making image part into component

--- resources/views/expenses/show.blade.php
+++ resources/views/expenses/show.blade.php
@@ -61,48 +61,7 @@
                     {{ $expense->purchased_date }}
             </x-show.data>
             <!-- div 9 file upload -->
-            <div
-                class="flex flex-col gap-4 items-start justify-between md:flex-row md:items-center md:gap-0"
-            >
-                <div>
-                    <h3 class="text-md font-bold mb-2 dark:text-gray-100">
-                        تصویر فاکتور و فیش پرداختی
-                    </h3>
-                    <p class="text-gray-500 text-sm dark:text-gray-300">
-                        تصویر فاکتور یا فیش پرداختی این هزینه
-                    </p>
-                </div>
-            </div>
-
-            <div class="relative mt-4 group overflow-hidden w-1/2 md:w-1/4">
-                <img
-                    class="border rounded p-4 shadow-md hover:scale-110 duration-300"
-                    src="/assets/images/logo.png"
-                />
-                <div
-                    class="hidden group-hover:flex gap-1 duration-200 absolute top-2 right-2"
-                >
-                    <a
-                        class="flex items-center justify-center w-[32px] h-[32px] rounded-full text-white bg-blue-500 hover:bg-blue-600 duration-200"
-                        href="#"
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke-width="1.5"
-                            stroke="currentColor"
-                            class="w-4 h-4 text-white dark:text-white"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-                            />
-                        </svg>
-                    </a>
-                </div>
-            </div>
+           <x-show.invoice :expense="$expense"  />
             <!-- mobile actions -->
             <div class="lg:hidden flex justify-end gap-3 mt-5">
                 <a
