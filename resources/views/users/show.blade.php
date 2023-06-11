688963b971c9c9599063dbb6c203199d7073ab5a
saeedshpc <saeedsh.dev@gmail.com>
2023-06-11 10:14:47 +0330

    fixing the duplication for image show component

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -53,7 +53,7 @@
             </x-show.data>
 
             <!-- div 7  -->
-            <x-show.profile-image :user="$user" />
+            <x-show.invoice :object="$user" />
 
             <!-- mobile actions -->
             <div class="lg:hidden flex justify-end gap-3 mt-5">
